38f81780cd367a95bdfbb3c58a0046bf4395e497
Saing Sab <saing.sab@gmail.com>
2021-06-16 16:31:50 +0700

    update source of contract

--- contracts/SEL.sol
+++ contracts/SEL.sol
@@ -1,9 +1,10 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.3;
+pragma solidity ^0.8.0;
 
-import "@OpenZeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Burnable.sol";
 
-contract SELENDRATOKEN is ERC20 {
+contract SELENDRATOKEN is ERC20, ERC20Burnable {
     constructor() ERC20("SELENDRA TOKEN", "SEL") {
         _mint(msg.sender, 3149592654 * (10**uint256(decimals())));
     }
