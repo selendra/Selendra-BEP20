dff8882f969e8bc85b133007fb45a7b20a5037fc
Saing Sab <saing.sab@gmail.com>
2021-06-16 18:08:47 +0700

    testnet and verified

--- contracts/SELToken.sol
+++ contracts/SELToken.sol
@@ -4,8 +4,8 @@ pragma solidity ^0.8.0;
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Burnable.sol";
 
-contract SELENDRATOKEN is ERC20, ERC20Burnable {
-    constructor() ERC20("SELENDRA TOKEN", "SEL") {
+contract SELToken is ERC20, ERC20Burnable {
+    constructor() ERC20("SELENDRA", "SEL") {
         _mint(msg.sender, 3149592654 * (10**uint256(decimals())));
     }
 }
--- truffle-config.js
+++ truffle-config.js
@@ -1,3 +1,6 @@
+require('dotenv').config();
+const HDWalletProvider = require('@truffle/hdwallet-provider');
+const mnemonic = process.env.MNEMONIC_DEV.toString();
 /**
  * Use this file to configure your truffle project. It's seeded with some
  * common settings for different networks and features like migrations,
@@ -72,6 +75,14 @@ module.exports = {
     // network_id: 2111,   // This network is yours, in the cloud.
     // production: true    // Treats this network as if it was a public net. (default: false)
     // }
+    bscTestnet: {
+      provider: () => new HDWalletProvider(
+        mnemonic,
+        'https://data-seed-prebsc-1-s1.binance.org:8545'
+      ),
+      network_id: 97,
+      skipDryRun: true
+    }
   },
 
   // Set default mocha options here, use special reporters etc.
@@ -82,7 +93,7 @@ module.exports = {
   // Configure your compilers
   compilers: {
     solc: {
-      version: "0.8.3",    // Fetch exact version from solc-bin (default: truffle's version)
+      version: "0.8.0",    // Fetch exact version from solc-bin (default: truffle's version)
       // docker: true,        // Use "0.5.1" you've installed locally with docker (default: false)
       // settings: {          // See the solidity docs for advice about optimization and evmVersion
       //  optimizer: {
@@ -92,6 +103,13 @@ module.exports = {
       //  evmVersion: "byzantium"
       // }
     }
+  },
+  plugins: [
+    'truffle-plugin-verify'
+  ],
+  api_keys: {
+    //bscscan: BSCSCANAPIKEY
+    bscscan: process.env.BSC_API_KEYS
   },
 
   // Truffle DB is currently disabled by default; to enable it, change enabled: false to enabled: true
